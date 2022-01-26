403262c2b14b21480c0c40955777788e4c87da9b
Viết Minh Hiếu <vietminhhieua5@gmail.com>
2022-01-26 21:29:03 +0700

    first time deploy

--- config/env.js
+++ config/env.js
@@ -2,10 +2,13 @@ require("dotenv/config");
 
 const env = {
   port: process.env.PORT || 4000,
-  database_connect: process.env.DB_CONNECT,
-  secret_token: process.env.SECRET_TOKEN,
-  admin_email: process.env.ARS_EMAIL,
-  admin_password: process.env.ARS_PASSWORD,
+  database_connect:
+    process.env.DB_CONNECT ||
+    "mongodb+srv://vmhieu198:vmhieu198@cluster0.4idy8.mongodb.net/ArsUserInfo?retryWrites=true&w=majority",
+  secret_token:
+    process.env.SECRET_TOKEN || "arsenalisthegreatestclubintheworld",
+  admin_email: process.env.ARS_EMAIL || "arsenalweb1@gmail.com",
+  admin_password: process.env.ARS_PASSWORD || "viethieu1982k",
 };
 
 module.exports = env;
