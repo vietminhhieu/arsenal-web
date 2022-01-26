f9b809cbb4a044301c920ff14ba0e315ad1e622a
Viết Minh Hiếu <vietminhhieua5@gmail.com>
2022-01-26 21:15:04 +0700

    make it better

--- config/env.js
+++ config/env.js
@@ -1,7 +1,7 @@
 require("dotenv/config");
 
 const env = {
-  port: process.env.PORT,
+  port: process.env.PORT || 4000,
   database_connect: process.env.DB_CONNECT,
   secret_token: process.env.SECRET_TOKEN,
   admin_email: process.env.ARS_EMAIL,
